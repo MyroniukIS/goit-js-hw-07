6305a6e0e8720150dfe7eaa80ac76f8f313cf640
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-08-08 05:38:49 +0300

    change task-05

---
+++ js/task-05.js
@@ -0,0 +1,18 @@
+//Напиши скрипт который, при наборе текста в инпуте 
+//input#name - input(событие input), подставляет его текущее значение 
+//в span#name - output.Если инпут пустой, 
+//в спане должна отображаться строка 'незнакомец'.
+
+const inputField = document.querySelector("input#name-input");
+const outputField = document.querySelector("span#name-output");
+
+const textInputOutput = () => {
+  const newText = inputField.value;
+  if (newValue) {
+    outputField.innerText = newText;
+  } else {
+    outputField.innerText = "незнакомец";
+  }
+};
+
+inputField.addEventListener("input", textInputOutput);
